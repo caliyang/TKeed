b9d2a58efc049ee79a592091bf0840e86d5dd51f
caliyang97 <caliyang97@gmail.com>
2021-03-24 16:19:10 +0800

    添加注释

--- src_code/threadpool.h
+++ src_code/threadpool.h
@@ -10,23 +10,27 @@
 #include <pthread.h>
 #include <stdint.h>
 
+/*task结构*/
 typedef struct tk_task{
     void (*func)(void*);
     void* arg;
     struct tk_task* next;    // 任务链表（下一节点指针）
 }tk_task_t;
 
+/* threadpool结构*/
 typedef struct threadpool{
-    pthread_mutex_t lock;    // 互斥锁
-    pthread_cond_t cond;    // 条件变量
-    pthread_t *threads;    // 线程
-    tk_task_t *head;    // 任务链表
+    pthread_mutex_t lock;    // 互斥锁；#include <pthreadtypes.h>，包含于#include <pthread.h>
+    pthread_cond_t cond;    // 条件变量；#include <pthreadtypes.h>，包含于#include <pthread.h>
+    pthread_t *threads;    // 线程；#include <pthreadtypes.h>，包含于#include <pthread.h>
+    tk_task_t *head;    // 任务链表（链表中某节点）
     int thread_count;    // 线程数 
     int queue_size;    // 任务链表长
     int shutdown;     // 关机模式
     int started;
 }tk_threadpool_t;
 
+
+/*threadpool_error 枚举类型别名*/
 typedef enum{
     tk_tp_invalid = -1,
     tk_tp_lock_fail = -2,
@@ -35,6 +39,7 @@ typedef enum{
     tk_tp_thread_fail = -5,
 }tk_threadpool_error_t;
 
+/*threadpool_sd 枚举类型别名*/
 typedef enum{
     immediate_shutdown = 1,
     graceful_shutdown = 2
